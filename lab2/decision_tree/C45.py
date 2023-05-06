3ced07f91469c4a2725bc7e0fb01e21892b9f539
Xyyyy123 <2210354438@qq.com>
2023-05-06 16:06:22 +0800

    final check & add comments

--- lab2/decision_tree/C45.py
+++ lab2/decision_tree/C45.py
@@ -1,3 +1,4 @@
+# 手写C45
 import numpy as np
 from tqdm import tqdm
 
@@ -12,17 +13,21 @@ class Node:
     def is_leaf_node(self):
         return self.value is not None
 
-class C45:
+class C45DecisionTree:
     def __init__(self, min_samples_split=2, max_depth=100):
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
         self.root = None
-
+    # 计算熵
     def _entropy(self, y):
+        # y中每个不同的标签出现的次数
         _, counts = np.unique(y, return_counts=True)
+        # 每个标签出现的概率
         probabilities = counts / len(y)
+        # 使用公式计算熵并返回
         return -np.sum(probabilities * np.log2(probabilities))
 
+    # 根据划分的阈值threshold将数据集划分为左右两个子集
     def _split(self, X, y, feature, threshold):
         left_mask = X[:, feature] < threshold
         right_mask = X[:, feature] >= threshold
@@ -32,7 +37,7 @@ class C45:
         best_gain_ratio = float('-inf')
         best_feature = None
         best_threshold = None
-        for feature in tqdm( range(X.shape[1])):
+        for feature in tqdm(range(X.shape[1])):
             thresholds = np.unique(X[:, feature])
             for threshold in thresholds:
                 X_left, y_left, X_right, y_right = self._split(X, y, feature, threshold)
@@ -50,6 +55,7 @@ class C45:
                     best_threshold = threshold
         return best_feature, best_threshold
 
+    # 递归构建决策树的函数
     def _build_tree(self, X, y, depth):
         n_samples, n_features = X.shape
         n_classes = len(np.unique(y))
@@ -61,19 +67,55 @@ class C45:
                 right_node = self._build_tree(X_right, y_right, depth + 1)
                 return Node(feature, threshold, left_node, right_node)
         return Node(value=np.argmax(np.bincount(y)))
-
-    def fit(self, X, y):
-        self.root = self._build_tree(X, y, 0)
-
+    # 对单个输入样本进行预测
     def _predict(self, x, node):
+        # 如果传入的节点是叶子节点，直接返回该叶子节点的值。
         if node.is_leaf_node():
             return node.value
+        # 如果输入样本在节点的分裂特征上的取值小于节点的阈值，则递归遍历左子树进行预测。
         if x[node.feature] < node.threshold:
             return self._predict(x, node.left)
+        # 否则，递归遍历右子树进行预测。
         return self._predict(x, node.right)
-
+    # 对数据集进行预测
     def predict(self, X):
         return np.array([self._predict(x, self.root) for x in X])
+    # 后剪枝方法
+    def _prune(self, node, X_val, y_val):
+        # 如果传入的节点是叶子节点
+        if node.is_leaf_node():
+            # 直接返回该叶子节点的值
+            return node.value
+
+        # 根据给定的特征数据和传入的节点，预测左右子树的预测结果
+        left_val = np.array([self._predict(x, node.left) for x in X_val])
+        right_val = np.array([self._predict(x, node.right) for x in X_val])
+
+        # 计算剪枝前的误差，使用错误分类的样本数量作为误差
+        error_before = np.sum(left_val != y_val[node.left]) + np.sum(right_val != y_val[node.right])
+        if not node.left.is_leaf_node():
+            node.left = Node(value=np.argmax(np.bincount(y_val[node.left])))
+        if not node.right.is_leaf_node():
+            node.right = Node(value=np.argmax(np.bincount(y_val[node.right])))
+        # 对左右子树分别进行剪枝
+        left_val = np.array([self._predict(x, node.left) for x in X_val])
+        right_val = np.array([self._predict(x, node.right) for x in X_val])
+        # 并计算剪枝后的误差
+        error_after = np.sum(left_val != y_val[node.left]) + np.sum(right_val != y_val[node.right])
+
+        # 如果剪枝后的误差小于等于剪枝前的误差，则保留剪枝后的决策树
+        if error_after <= error_before:
+            return node.value
+        # 否则，恢复原始的决策树结构，递归剪枝左右子树
+        node.left = self._prune(node.left, X_val, y_val)
+        node.right = self._prune(node.right, X_val, y_val)
+        return node
+
+    def fit(self, X, y, X_val=None, y_val=None):
+        self.root = self._build_tree(X, y, 0)
+        if X_val is not None and y_val is not None:
+            self.root = self._prune(self.root, X_val, y_val)
+
 if __name__ == "__main__":
     from sklearn.tree import DecisionTreeClassifier
     from sklearn.metrics import accuracy_score
@@ -81,12 +123,12 @@ if __name__ == "__main__":
     # 划分训练集和测试集
     X_train, y_train, X_test, y_test = load_cora()
     # 使用手写实现的C45
-    our_nb = C45(max_depth=3)
+    our_nb = C45DecisionTree(max_depth=3)
     our_nb.fit(X_train, y_train)
     our_predictions = our_nb.predict(X_test)
 
     # 使用sklearn的朴素贝叶斯分类器
-    sklearn_nb = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=42)
+    sklearn_nb = DecisionTreeClassifier(criterion='entropy', max_depth=3)
     sklearn_nb.fit(X_train, y_train)
     sklearn_predictions = sklearn_nb.predict(X_test)
 
--- lab2/decision_tree/CART.py
+++ lab2/decision_tree/CART.py
@@ -1,9 +1,12 @@
+# 手写CART
 import numpy as np
 from tqdm import tqdm
 
-class DecisionTree:
+class CartDecisionTree:
     def __init__(self, max_depth=None, min_samples_split=2):
+        # 最大深度
         self.max_depth = max_depth
+        # 小拆分样本数
         self.min_samples_split = min_samples_split
 
     def fit(self, X, y):
@@ -22,54 +25,83 @@ class DecisionTree:
 
         return y_pred
 
+    # 递归构建决策树
     def _build_tree(self, X, y, depth):
+        # 如果达到最大深度或样本数少于最小分裂样本数，返回叶节点，并以最常见的标签作为该叶节点的预测值
         if depth == self.max_depth or len(X) < self.min_samples_split:
             return {'is_leaf': True, 'label': self._most_common_label(y)}
+        # 如果所有样本都属于同一类别，返回叶节点，并以该类别作为该叶节点的预测值
         if self._all_same_class(y):
             return {'is_leaf': True, 'label': y[0]}
-        split_feature, split_value = self._find_best_split(X, y)
+        # 选择最佳拆分特征和拆分点，根据拆分点将数据集分成左右两个子集
+        split_feature, split_value = self._best_split(X, y)
         left_indices = X[:, split_feature] <= split_value
         right_indices = X[:, split_feature] > split_value
-        left_subtree = self._build_tree(X[left_indices], y[left_indices], depth=depth+1)
-        right_subtree = self._build_tree(X[right_indices], y[right_indices], depth=depth+1)
+        # 递归构建左子树和右子树
+        left_subtree = self._build_tree(X[left_indices], y[left_indices], depth=depth + 1)
+        right_subtree = self._build_tree(X[right_indices], y[right_indices], depth=depth + 1)
+        # 返回非叶节点，并记录拆分特征、拆分点以及左右子树
         return {'is_leaf': False, 'split_feature': split_feature, 'split_value': split_value,
                 'left': left_subtree, 'right': right_subtree}
 
-    def _find_best_split(self, X, y):
+    # 找到最佳拆分特征和拆分值，以最大化信息增益
+    def _best_split(self, X, y):
+        # 初始化最佳拆分特征、拆分值和信息增益
         best_split_feature = None
         best_split_value = None
         best_info_gain = -float('inf')
+        # 遍历所有特征
         for feature in tqdm(range(X.shape[1])):
+            # 对当前特征的取值进行排序
             values = sorted(list(set(X[:, feature])))
+            # 遍历当前特征的所有取值
             for i in tqdm(range(1, len(values))):
-                split_value = (values[i-1] + values[i]) / 2
+                # 计算当前取值和上一个取值的平均值作为拆分点
+                split_value = (values[i - 1] + values[i]) / 2
+                # 根据拆分点将数据集分成左右两个子集
                 left_indices = X[:, feature] <= split_value
                 right_indices = X[:, feature] > split_value
+                # 如果有一个子集为空，则跳过这个拆分点
                 if len(left_indices) == 0 or len(right_indices) == 0:
                     continue
+                # 计算通过当前拆分点得到的信息增益
                 info_gain = self._information_gain(y, y[left_indices], y[right_indices])
+                # 如果当前信息增益大于最大信息增益，则更新最佳拆分特征、拆分值和信息增益
                 if info_gain > best_info_gain:
                     best_info_gain = info_gain
                     best_split_feature = feature
                     best_split_value = split_value
+        # 返回最佳拆分特征和拆分值
         return best_split_feature, best_split_value
 
+    # 计算信息增益
     def _information_gain(self, y, y_left, y_right):
+
+        # 左子树样本数占总样本数的比例
         p = len(y_left) / len(y)
+        # 总体数据集的熵
         entropy_parent = self._entropy(y)
+        # 分裂后两个子节点的加权平均熵
         entropy_children = p * self._entropy(y_left) + (1 - p) * self._entropy(y_right)
+        # 返回信息增益 = 总体数据集的熵 - 分裂后两个子节点的加权平均熵
         return entropy_parent - entropy_children
 
+    # 计算输入标签y的熵
     def _entropy(self, y):
+        # 统计每个类别出现的次数
         _, counts = np.unique(y, return_counts=True)
+        # 计算每个类别的出现概率
         probabilities = counts / len(y)
+        # 根据公示计算熵并返回
         return -np.sum(probabilities * np.log2(probabilities))
 
+    # 计算标签y中最常见的类别
     def _most_common_label(self,y):
         unique_labels, counts = np.unique(y, return_counts=True)
         index = np.argmax(counts)
         return unique_labels[index]
 
+    # 判断标签y中的所有样本是否属于同一类别
     def _all_same_class(self, y):
         _, counts = np.unique(y, return_counts=True)
         return len(counts) == 1
@@ -81,7 +113,7 @@ if __name__ == "__main__":
     # 划分训练集和测试集
     X_train, y_train, X_test, y_test = load_cora()
     # 使用手写实现的CART
-    our_nb = DecisionTree(max_depth=3)
+    our_nb = CartDecisionTree(max_depth=3)
     our_nb.fit(X_train, y_train)
     our_predictions = our_nb.predict(X_test)
 
@@ -98,4 +130,4 @@ if __name__ == "__main__":
     print(f"Sklearn CART DecisionTree Classifier accuracy: {sklearn_accuracy}")
 
     # Our CART DecisionTree Classifier accuracy: 0.359
-    # sklearn CART DecisionTree Classifier accuracy: 0.359
+    # sklearn CART DecisionTree Classifier accuracy: 0.336
